145195b2d9538ce7409017fb98955c73080f419d
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-02-21 17:30:49 +0000

    Refactor axios implementation when fetching data

--- src/ApiTemplates/BoredAPICaller.js
+++ src/ApiTemplates/BoredAPICaller.js
@@ -5,20 +5,18 @@ function BoredAPICaller(props){
     const expressServerBoredApiUrl="http://localhost:5000/boredApi"
 
     useEffect(()=>{
-        const boredApiData= async () =>{
-            await axios.get(expressServerBoredApiUrl, {
-            params: {
-                participants:props.numberOfParticipants
-            }})
-        .then(response =>{
-            // console.log(response.data.activity+response.data.type+response.data.participants);
-            props.userRecommendationsFromApi(response.data.activity,response.data.type);
-        })
-        .catch(error=>{
-            console.log(error.message);
-        })
-    } 
-        boredApiData()
+        ( async () =>{
+            try{
+                const boredApiData= await axios.get(expressServerBoredApiUrl, {
+                    params: {
+                        participants:props.numberOfParticipants
+            }}) 
+            console.log(boredApiData.data.activity+boredApiData.data.type+boredApiData.data.participants);
+            props.userRecommendationsFromApi(boredApiData.data.activity,boredApiData.data.type);
+        } catch (error){
+            console.log(error.message)
+            }
+        })();
     },[props.isSubmitPressed]); // Removing the array doesn't work as no element here will 
                                 // undergo re-rendering as the useEffect would never be called. Hence
                                 // the props.attribute is used to fetch API when those changes
@@ -31,4 +29,4 @@ export default BoredAPICaller;
 
 //useEffect: Function inside this hook is automatically rendered when the DOM renders
 // Three ways, first: every time DOM renders, second: when value inside array changes
-// third; only on first rendering by declaring an empty array 
+// third; only on first rendering by declaring an empty array
