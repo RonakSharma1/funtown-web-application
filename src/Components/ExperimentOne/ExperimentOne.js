6fd0581933a08d23a0522e3c52a6052f1a68cebc
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-03-02 20:22:58 +0000

    Apply prettier to improve code formatting

--- src/Components/ExperimentOne/ExperimentOne.js
+++ src/Components/ExperimentOne/ExperimentOne.js
@@ -1,15 +1,14 @@
-import ExperimentOneHeader from './ExperimentOneHeader.js'
-import ExperimentOneUserForm from './ExperimentOneUserForm.js'
+import ExperimentOneHeader from "./ExperimentOneHeader.js";
+import ExperimentOneUserForm from "./ExperimentOneUserForm.js";
 
-function ExperimentOne(props){
-
-    return(
-        <div>
-            <ExperimentOneHeader/>
-            <ExperimentOneUserForm/>
-            <button onClick={props.isBackButtonClicked}>Back to Main Arena</button>
-        </div>
-    );
+function ExperimentOne(props) {
+  return (
+    <div>
+      <ExperimentOneHeader />
+      <ExperimentOneUserForm />
+      <button onClick={props.isBackButtonClicked}>Back to Main Arena</button>
+    </div>
+  );
 }
 
 export default ExperimentOne;
--- src/Components/MainApp/MainAppHeader.js
+++ src/Components/MainApp/MainAppHeader.js
@@ -1,22 +1,33 @@
-import '../../CSS/MainApp/MainAppHeader.css';
-import party from '../../Media/party.gif'
+import "../../CSS/MainApp/MainAppHeader.css";
+import party from "../../Media/party.gif";
 
-function Header (props) {
-    return(
-        <div className="header">
-            <img src={party} className="party_image_right" alt="Minions dancing in a party" />
-            <img src={party} className="party_image_left" alt="Minions dancing in a party" />
-            <h1>Welcome to the FunTown!</h1>
-            <a
-                className="header"
-                href="https://youtu.be/k85mRPqvMbE"
-                target="_blank"
-                rel="noreferrer"
-            >
-                Synergising Fun and Work
-            </a>
-            <p>Aim: To experiment with React, Axios and Express for understanding web development</p>
-        </div>
-    );
+function Header(props) {
+  return (
+    <div className="header">
+      <img
+        src={party}
+        className="party_image_right"
+        alt="Minions dancing in a party"
+      />
+      <img
+        src={party}
+        className="party_image_left"
+        alt="Minions dancing in a party"
+      />
+      <h1>Welcome to the FunTown!</h1>
+      <a
+        className="header"
+        href="https://youtu.be/k85mRPqvMbE"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Synergising Fun and Work
+      </a>
+      <p>
+        Aim: To experiment with React, Axios and Express for understanding web
+        development
+      </p>
+    </div>
+  );
 }
 export default Header;
