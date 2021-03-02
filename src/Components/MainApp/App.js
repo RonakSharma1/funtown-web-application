dcc5c22b45a1e6ba5b3b9655a6e0eb12154a6ffa
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-03-02 20:22:10 +0000

    Optimse code for efficiency

--- src/Components/MainApp/App.js
+++ src/Components/MainApp/App.js
@@ -1,43 +1,48 @@
-import '../../CSS/MainApp/App.css';
-import React, { useState } from 'react';
-import Header from './MainAppHeader.js';
-import ExperimentOne from '../ExperimentOne/ExperimentOne.js';
-import ExperimentTwo from '../ExperimentTwo/ExperimentTwo.js';
-
+import "../../CSS/MainApp/App.css";
+import React, { useState } from "react";
+import Header from "./MainAppHeader.js";
+import ExperimentOne from "../ExperimentOne/ExperimentOne.js";
+import ExperimentTwo from "../ExperimentTwo/ExperimentTwo.js";
 
 function App() {
   // Button States
-  const[experimentButtonState,setExperimentButtonState]=useState('0')
-  const experimentButtons=[
+  const [experimentButtonState, setExperimentButtonState] = useState("0");
+  const experimentButtons = [
     {
-      name: 'Experiment 1',
-      id : '1'
+      name: "Experiment 1",
+      id: "1",
     },
     {
-      name: 'Experiment 2',
-      id:'2'
-    }
+      name: "Experiment 2",
+      id: "2",
+    },
   ];
 
-  const handleExperimentButtonState = (experimentButtonSelected)=>{
-    setExperimentButtonState(experimentButtonSelected);
-  }
-  
   return (
+    <div>
       <div>
-        <div>
-          {(experimentButtonState === '0') && <Header/>}
-          {(experimentButtonState === '1') && <ExperimentOne isBackButtonClicked={()=>{setExperimentButtonState('0')}}/>}
-          {(experimentButtonState === '2') && <ExperimentTwo />}
-        </div>
-        <div>
-          {
-          experimentButtons.map( button => 
-                  (experimentButtonState === '0') && 
-                  <button key={button.id} onClick={()=>{handleExperimentButtonState(button.id)}}>{button.name}</button>)
-          }
-        </div>
+        {experimentButtonState === "0" && <Header />}
+        {experimentButtonState === "1" && (
+          <ExperimentOne
+            isBackButtonClicked={() => setExperimentButtonState("0")}
+          />
+        )}
+        {experimentButtonState === "2" && <ExperimentTwo />}
       </div>
+      <div>
+        {experimentButtons.map(
+          (button) =>
+            experimentButtonState === "0" && (
+              <button
+                key={button.id}
+                onClick={() => setExperimentButtonState(button.id)}
+              >
+                {button.name}
+              </button>
+            )
+        )}
+      </div>
+    </div>
   );
 }
 export default App;
