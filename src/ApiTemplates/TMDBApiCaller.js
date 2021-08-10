25cda194a17644d0c7058d940af865d70ef23d6a
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-08-10 21:58:50 +0100

    Call tmdb express server

--- src/ApiTemplates/TMDBApiCaller.js
+++ src/ApiTemplates/TMDBApiCaller.js
@@ -1,21 +1,16 @@
 import axios from "axios";
-var config = require("../config");
 
 export default async (pageNumber) => {
-  const tmdbApiKey = config.TMDB.tmdbApiKey;
-  const expressServerTMDBApiUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${tmdbApiKey}&page=${pageNumber}`;
-  const expressServerTMDBGenreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${tmdbApiKey}`;
-
+  const expressServerTmdbApiUrl = "http://localhost:5000/tmdbApi";
   try {
-    const [tmdbApiData, tmbdGenreData] = await Promise.all([
-      axios.get(expressServerTMDBApiUrl),
-      axios.get(expressServerTMDBGenreUrl),
-    ]);
-    const arrayOfMovies = tmdbApiData.data.results;
-    const arrayOfGenres = tmbdGenreData.data.genres;
+    const tmdbApiData = await axios.get(expressServerTmdbApiUrl, {
+      params: {
+        pageNumber: pageNumber,
+      },
+    });
     return {
-      listOfTrendingFilms: arrayOfMovies,
-      listOfGenres: arrayOfGenres,
+      listOfTrendingFilms: tmdbApiData.data.listOfTrendingFilms,
+      listOfGenres: tmdbApiData.data.listOfGenres,
     };
   } catch (error) {
     console.log(error.message);
